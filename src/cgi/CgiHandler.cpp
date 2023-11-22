c1bd3ffb0c8889aa7599bf2ce0edff8796b79400
Pedro Manuel Pereira Coelho Bigote <pedperei@c1r13s1.42lisboa.com>
2023-11-22 18:51:35 +0000

    CGI executor in the making

--- src/cgi/CgiHandler.cpp
+++ src/cgi/CgiHandler.cpp
@@ -18,3 +18,63 @@ void CgiHandler::initCgi_Env(HttpRequest& request, HttpServer& server, RouteHand
         // Add any other CGI environment variables your script might need
  */
 }
+
+void CgiHandler::executeCgi_Script()
+{
+    fd = open();
+    pipe(tubes);
+    if ((cgi_pid = fork())== 0) // child process
+    {
+        close(tubes[1]);  
+        dup2(tubes[0], 0);   
+        execve(args[0], args, env);
+    }
+}
+void             ft::CGI::execute(HttpRequest& request, int fd, )
+{
+    pid_t   pid;
+    int     pipe_in[2]
+    int     pipe_out[2];
+    int     status;
+
+    if (pipe(pipe_in) < 0)
+    {
+        std::cerr << "pipe failed\n";
+        response.setStatusCode("500");
+        return ;
+    }
+    if (pipe(pipe_out) < 0)
+    {
+        std::cerr << "pipe failed\n";
+        close(pipe_in[0]);
+        close(pipe_in[1]);
+        response.setStatusCode("500");
+        return ;
+    }
+    pid = fork();
+    if (pid == 0)
+    {
+        dup2(pipe_in[0], STDIN_FILENO);
+        dup2(pipe_out[1], STDOUT_FILENO);
+        write(pipe_in[1], request.getBody().c_str(), atoi(this->cgi_Env["CONTENT_LENGTH"].c_str()));
+        close(pipe_in[0]);
+        //close(pipe_in[1]);
+        //close(pipe_out[0]);
+        close(pipe_out[1]);
+        status = execve(_argv[0], _argv, _cenv);
+        response.setStatusCode("500");
+        exit(status);
+    }
+    else if (pid > 0){
+
+        close(pipe_in[1]);
+        waitpid(pid, &status, 0);
+        close(pipe_out[1]);
+        if (status < 0)
+        {
+            close(pipe_out[0]);
+            close(pipe_in[0]);
+            return ;
+        }
+
+}
